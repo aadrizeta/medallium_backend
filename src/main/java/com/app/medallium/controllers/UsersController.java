ad8ab2e31ac12c44e123b13e7324ca4880182e37
Javier <Javierr2305@gmail.com>
2025-02-01 18:32:49 +0100

    añadido el servicio de user aun por probar funcionalidad cuando este el fronto correcto

--- src/main/java/com/app/medallium/controllers/UsersController.java
+++ src/main/java/com/app/medallium/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.app.medallium.controllers;
 
 import com.app.medallium.models.Users;
+import com.app.medallium.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +14,13 @@ import java.util.List;
 public class UsersController {
 
     @Autowired
-    private UsersController usersController;
+    private UserService usersService;
 
     // localhost:8080/api/users -> si no hay parámetro en GetMapping
     // localhost:8080/api/users/get-users
     @GetMapping("/get-users")
     public ResponseEntity<List<Users>> getAllUsers() {
-        List<Users> users = (List<Users>) this.usersController.getAllUsers();
+        List<Users> users = this.usersService.getAllUsers();
         return ResponseEntity.ok(users);
     }
 
@@ -27,7 +28,7 @@ public class UsersController {
     // localhost:8080/api/users/create
     @PostMapping("/create")
     public ResponseEntity<Users> createUser(@RequestBody Users user) {
-        Users createdUser = usersController.createUser(user).getBody();
+        Users createdUser = usersService.createUser(user);
         return ResponseEntity.ok(createdUser);
     }
 
